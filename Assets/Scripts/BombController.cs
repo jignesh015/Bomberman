5b153da13ecd68003e292a99bcdbee6f4c57b047
Jignesh Patel <patel.jignesh705@gmail.com>
2019-11-27 22:16:09 +0530

    Added player movement and bomb spawning

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class BombController : MonoBehaviour
 {
+    private Transform player;
+    private bool shouldTween = true;
+    private bool isRigid = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (shouldTween)
+        {
+            shouldTween = false;
+            transform.DOPunchScale(new Vector3(-0.1f, -0.1f, -0.1f), 0.3f, 1, 0.2f).OnComplete(() => { shouldTween = true; });
+        }
+
+        if (!isRigid)
+        {
+            if (Vector3.Distance(player.position, transform.position) > 1)
+            {
+                isRigid = true;
+                GetComponent<BoxCollider>().enabled = true;
+            }
+        }
+
     }
+
+
 }

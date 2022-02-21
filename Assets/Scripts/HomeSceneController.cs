2a461e0e361754f2b554609012d8e73ab34171ad
Jignesh <patel.jignesh705@gmail.com>
2022-02-21 20:54:20 +0530

    Added Home Screen

---
+++ Assets/Scripts/HomeSceneController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeSceneController : MonoBehaviour
+{
+    [Header("Button References")]
+    [SerializeField] private List<GameObject> buttons;
+    [SerializeField] private GameObject controlPanel;
+    [SerializeField] private GameObject howToPlayPanel;
+    [SerializeField] private GameObject creditsPanel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Show Continue button if completed few levels before
+        buttons[0].SetActive(PlayerPrefs.HasKey("Level_Completed"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnContinueGamePressed()
+    { 
+    
+    }
+
+    public void OnNewGamePressed()
+    {
+        int _levelNo = PlayerPrefs.GetInt("Level_Completed");
+        if (_levelNo == 0) _levelNo = 1;
+        MultiSceneManager.Instance.StartGame(_levelNo);
+    }
+
+    public void OnHowToPlayPressed()
+    {
+        CloseAllCenterPanels();
+        howToPlayPanel.SetActive(true);
+    }
+
+    public void OnControlsPressed()
+    {
+        CloseAllCenterPanels();
+        controlPanel.SetActive(true);
+    }
+
+    public void OnCreditsPressed()
+    {
+        CloseAllCenterPanels();
+        creditsPanel.SetActive(true);
+    }
+
+    public void OnQuitPressed()
+    {
+        Application.Quit();
+    }
+
+    private void CloseAllCenterPanels()
+    {
+        controlPanel.SetActive(false);
+        howToPlayPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/MultiSceneManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MultiSceneManager : MonoBehaviour
+{
+    [Header("UI References")]
+    [SerializeField] private GameObject loadingScreen;
+
+    private static MultiSceneManager _instance;
+
+    public static MultiSceneManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadingScreen.SetActive(true);
+        LoadHomeScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleLoadingScreen(bool _toggleOn)
+    {
+        loadingScreen.SetActive(_toggleOn);
+    }
+
+    public void LoadHomeScene()
+    {
+        StartCoroutine(LoadHomeSceneAsync());
+    }
+
+    private IEnumerator LoadHomeSceneAsync()
+    {
+        ToggleLoadingScreen(true);
+        yield return new WaitForSeconds(2f);
+        yield return StartCoroutine(LoadSceneAdditive("Home"));
+        ToggleLoadingScreen(false);
+    }
+
+    public void StartGame(int _levelNo)
+    {
+        StartCoroutine(StartGameAsync(_levelNo));
+    }
+
+    private IEnumerator StartGameAsync(int _levelNo)
+    {
+        string _levelName = string.Format("Level {0}", _levelNo);
+        ToggleLoadingScreen(true);
+        yield return StartCoroutine(UnloadScene("Home"));
+        yield return StartCoroutine(LoadSceneAdditive(_levelName));
+        ToggleLoadingScreen(false);
+    }
+
+    private IEnumerator LoadSceneAdditive(string _sceneName)
+    {
+        Scene _scene = SceneManager.GetSceneByName(_sceneName);
+        if (_scene == null) yield break;
+        if (_scene.isLoaded) yield break;
+
+        AsyncOperation asyncAROperation = SceneManager.LoadSceneAsync(_sceneName, LoadSceneMode.Additive);
+        asyncAROperation.allowSceneActivation = false;
+        while (!asyncAROperation.isDone)
+        {
+            if (asyncAROperation.progress >= 0.9f)
+            {
+                asyncAROperation.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+
+    private IEnumerator UnloadScene(string _sceneName)
+    {
+        Scene _scene = SceneManager.GetSceneByName(_sceneName);
+        if (_scene != null && _scene.isLoaded)
+        {
+            AsyncOperation unloadAsync = SceneManager.UnloadSceneAsync(_sceneName, UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
+            while (!unloadAsync.isDone)
+            {
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+
+}
